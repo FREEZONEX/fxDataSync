f0070df4f354bf8e0ec52e1f635fec794c6ff583
nishenghao <1159417019@qq.com>
2024-11-25 17:18:47 +0800

    测试类更新

--- src/test/java/org/niiish32x/fxdemo/FxdemoApplicationTests.java
+++ src/test/java/org/niiish32x/fxdemo/FxdemoApplicationTests.java
@@ -1,13 +1,14 @@
 package org.niiish32x.fxdemo;
 
 import org.junit.jupiter.api.Test;
-import org.niiish32x.fxdemo.http.HttpUtil;
 import org.niiish32x.fxdemo.service.DataSendService;
-import org.niiish32x.fxdemo.service.ReadJsonTool;
+import org.niiish32x.fxdemo.tools.Common;
+import org.niiish32x.fxdemo.tools.ReadJsonTool;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.concurrent.CompletableFuture;
+import java.io.IOException;
 
 @SpringBootTest
 class FxdemoApplicationTests {
@@ -16,15 +17,16 @@ class FxdemoApplicationTests {
     DataSendService dataSendService;
 
 
+
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException, InterruptedException {
 
-        for (String rule : ReadJsonTool.RULE_COLLECTION) {
-//             dataSendService.sendToSupos(HttpUtil.TEST_URL,HttpUtil.TEST_PREFIX,rule);
-//             dataSendService.sendToSupos(HttpUtil.PROD_URL,HttpUtil.PROD_PREFIX,rule);
+        dataSendService.postToSuposTest(ReadJsonTool.lmsSugarJsonData, Common.PREFIX_SUGAR_AREA_TEST);
+        dataSendService.postToSuposTest(ReadJsonTool.lmsPowerJsonData, Common.PREFIX_POWER_AREA_TEST);
+        dataSendService.postToSuposTest(ReadJsonTool.lmsSugarJsonData, Common.PREFIX_DISTILLERY_AREA_TEST);
+        dataSendService.postToSuposTest(ReadJsonTool.lmsGrainJsonData, Common.PREFIX_Grains_Production_Summary_On_Maize_Rice_TEST);
 
-        }
-//        dataSendService.sendToSupos(HttpUtil.TEST_URL,HttpUtil.TEST_PREFIX,ReadJsonTool.lmsPowerJsonData);
     }
 
 }
